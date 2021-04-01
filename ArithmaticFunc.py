5affbbf9fc239c9af7159082e0deffb2bf715b89
Amruta Kulkarni <amyakulkarni1998@gmail.com>
2021-04-01 20:37:46 +0530

    Arithmetic operation

---
+++ ArithmaticFunc.py
@@ -0,0 +1,28 @@
+def add(a,b):
+    res = a + b
+    print(res)
+add(2, 4)
+
+def sub(a,b):
+    res = a - b
+    print(res)
+
+sub(5, 1)    
+
+def sub(a,b):
+    res = a * b
+    print(res)
+
+sub(5, 1)    
+
+def sub(a,b):
+    res = a / b
+    print(res)
+
+sub(5, 1)
+
+def sub(a,b):
+    res = a % b
+    print(res)
+
+sub(5, 3)    
